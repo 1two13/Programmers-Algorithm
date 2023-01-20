1b83fe0d05acf879efc5782fe133f0a31347073a
Tory <88531407+1two13@users.noreply.github.com>
2023-01-20 19:01:08 +0900

    [재시도/X] 구명보트.js

--- Lv.2/😵/구명보트.js
+++ Lv.2/😵/구명보트.js
@@ -1,9 +1,29 @@
-// 보트에 최대 2명 탑승 가능 + 무게 제한
+//!나의 코드(35.0점)
+function solution(people, limit) {
+  // 구명보트 개수의 최솟값
+  let answer = 0;
+  people.sort((a, b) => b - a);
+
+  while (people.length > 0) {
+    let rest = limit - people[0];
+    let arr = people.filter((value) => value <= rest);
+
+    if (arr.length !== 0) {
+      for (let i = 1; i < people.length; i++) {
+        if (rest >= people[i]) people.splice(i, 1);
+      }
+    }
+    answer++;
+    people.shift();
+  }
+
+  return answer;
+}
+
 //!다른 분 코드
 function solution(people, limit) {
   // 내림차순 정렬(작은 => 큰)
   people.sort((a, b) => a - b);
-  // console.log(people)
 
   let i = 0;
   for (j = people.length - 1; i < j; j--) {
