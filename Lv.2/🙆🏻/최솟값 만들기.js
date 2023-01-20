500cd2dfa21121161bdb37501d40704a253e68c9
Tory <88531407+1two13@users.noreply.github.com>
2023-01-20 13:50:33 +0900

    [재시도/O] 최솟값 만들기.js

--- Lv.2/🙆🏻/최솟값 만들기.js
+++ Lv.2/🙆🏻/최솟값 만들기.js
@@ -1,14 +1,11 @@
 //!나의 코드
-// 재귀로 풀어야한다고 생각해서 식을 써봤는데 도저히 해답이 나오지 않았다..
-
-//!다른 분 코드
 function solution(A, B) {
+  // 두 수를 곱한 값을 누적하여 더한 값의 최소값
   let answer = 0;
 
   A = A.sort((a, b) => a - b);
   B = B.sort((a, b) => b - a);
 
-  // A배열에서 가장 작은 수와 B배열에서 가장 큰 수를 곱하기
   for (let i = 0; i < A.length; i++) answer += A[i] * B[i];
 
   return answer;
