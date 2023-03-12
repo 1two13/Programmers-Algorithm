d405b371b6ff482a3e01a07ff9e1b75bb09fa74f
Tory <88531407+1two13@users.noreply.github.com>
2023-03-12 19:04:21 +0900

    할인 행사.js 빈 행 추가

--- Lv.2/😵/할인 행사.js
+++ Lv.2/😵/할인 행사.js
@@ -26,17 +26,20 @@ function solution(want, number, discount) {
 //!다른 분 코드
 function solution(want, number, discount) {
   let count = 0;
+
   for (let i = 0; i < discount.length - 9; i++) {
     const slice = discount.slice(i, i + 10);
-
     let flag = true;
+
     for (let j = 0; j < want.length; j++) {
       if (slice.filter((item) => item === want[j]).length !== number[j]) {
         flag = false;
         break;
       }
     }
+
     if (flag) count += 1;
   }
+
   return count;
 }
